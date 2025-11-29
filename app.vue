1b59b49b97673b830ef479b1a303bc86ae2c42e5
Sea <zzznn.chen@gmail.com>
2025-11-29 17:32:23 +0800

    新增tailwindcss

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class=" mt-4">
+    <div class="text-2xl text-red-600 ">
+      Welcome to Your Nuxt 3 App
+    </div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
