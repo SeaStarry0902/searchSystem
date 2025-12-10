27dadad9a1b26d41b7051cf0fd7845ea18f394eb
Sea <zzznn.chen@gmail.com>
2025-12-10 23:41:38 +0800

    完成查詢功能

--- pages/courseSearch.vue
+++ pages/courseSearch.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { ref } from 'vue'
 import testInput from '~/components/TextInput.vue'
+let params = []
+let newURL = ''
+let temDay = 1
+let temSec = 1
 const token = useCookie('access_token')
 const type = useCookie('token_type')
-const courseArray = ['編號', '學期', '系所', '年級', '班組', '科目分類', '課程名稱', '教師', '上課人數', '學分數', '課別', '節次', '收藏']
+const courseArray = ['編號', '學期', '系所', '年級', '課程名稱', '教師', '上課人數', '學分', '課別', '地點', '星期', '節次', '收藏']
 const weekArray = ["一", "二", "三", "四", "五", "六", "日"]
 const result = []
+let favoriteArray = ref([])
+const courseIdArray = []
 const apiResponse = ref(null)
 const keyword = ref('')
 const semester = ref('')
@@ -14,8 +20,22 @@ const grade = ref(null)
 const teacher = ref('')
 const catagory = ref('')
 const department = ref('')
-const baseURL = `${token.value? 'https://representative-winni-chongouo-b8ca194b.koyeb.app/courses?' : 'https://representative-winni-chongouo-b8ca194b.koyeb.app/courses/public?'}`
-let newURL = ''
+const showTable = ref(false)
+const showFilters = ref(true)
+const excelBlob = ref(null)
+const showChooceTime = ref(false)
+const baseExcelURL = 'https://representative-winni-chongouo-b8ca194b.koyeb.app/courses/export'
+const baseURL = `${token.value ? 'https://representative-winni-chongouo-b8ca194b.koyeb.app/courses?' : 'https://representative-winni-chongouo-b8ca194b.koyeb.app/courses/public?'}`
+const weekResult = ref(
+    Object.fromEntries(
+        weekArray.map(day => [
+            day,
+            Object.fromEntries(
+                Array.from({ length: 14 }, (_, i) => [i + 1, false])
+            )
+        ])
+    )
+)
 const filters = {
     keyword,
     semester,
@@ -27,7 +47,7 @@ const filters = {
 }
 function buildUrl() {
     newURL = baseURL
-    const params = []
+    params = []
     for (const item in filters) {
         if (filters[item].value) {
             params.push(`${item}=${filters[item].value}`)
@@ -38,23 +58,9 @@ function buildUrl() {
             params.push(`time_slots=${result[item]}`)
         }
     }
-    newURL += `${params.join('&')}&page=1&page_size=20`
-    console.log(newURL)
+    newURL += `${params.join('&')}&page=1&page_size=60`
+    console.log('這是查詢用的' + newURL)
 }
-const showChooceTime = ref(false)
-let temDay = 1
-let temSec = 1
-const weekResult = ref(
-    Object.fromEntries(
-        weekArray.map(day => [
-            day,
-            Object.fromEntries(
-                Array.from({ length: 14 }, (_, i) => [i + 1, false])
-            )
-        ])
-    )
-);
-
 function toggleDay(day) {
     const checked = Object.values(weekResult.value[day]).every(v => v === true);
     const newValue = !checked;
@@ -86,20 +92,79 @@ function saveResult() {
     console.log(result)
     showChooceTime.value = false
 }
+function showResult() {
+    showTable.value = true
+    let idx = 1
+    let lastId = ''
+    for (let key = 0; key < apiResponse.value.items.length; key++) {
+        if (apiResponse.value.items[key].id === lastId)
+            continue
+        const temData = apiResponse.value.items[key]
+        // console.log(apiResponse.value.items[key].name_zh)
+        courseArray.push(idx)
+        courseArray.push(temData.semester)
+        courseArray.push(temData.department_name)
+        courseArray.push(temData.grade)
+        courseArray.push(temData.name_zh)
+        courseArray.push(temData.teacher_name)
+        courseArray.push(temData.limit_max)
+        courseArray.push(temData.credit)
+        courseArray.push(temData.required_type)
+        courseArray.push(temData.times[0].classroom)
+        courseArray.push(temData.times[0].weekday)
+        courseArray.push(`${temData.times[0].start_section === temData.times[0].end_section ? temData.times[0].start_section : temData.times[0].start_section + '~' + temData.times[0].end_section}節`)
+        courseArray.push(temData.is_favorite)
+        favoriteArray.value.push(temData.is_favorite)
+        courseIdArray.push(temData.id)
+        idx++
+        lastId = temData.id
+    }
+}
+async function downloadExcel() {
+    newURL = baseExcelURL
+    if (params.length !== 0)
+        newURL += `?${params.join('&')}`
+    console.log('這是excel的' + newURL)
+    excelBlob.value = await $fetch(newURL, {
+        responseType: 'blob',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const url = URL.createObjectURL(excelBlob.value)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = '課程資料.xlsx'
+    a.click()
+    URL.revokeObjectURL(url)
+}
 async function courseSearch() {
     buildUrl()
-    apiResponse.value =await $fetch(newURL, {
+    apiResponse.value = await $fetch(newURL, {
         headers: {
             'Authorization': type.value + ' ' + token.value,
             'Content-Type': 'application/json'
         }
     })
-    console.log('回傳結果:',apiResponse.value)
+    console.log('回傳結果:', apiResponse.value)
+    showFilters.value = false
+    showResult()
+}
+async function addFavorite(idx) {
+    favoriteArray.value[idx] = true
+    await $fetch(`https://representative-winni-chongouo-b8ca194b.koyeb.app/favorites/${courseIdArray[idx]}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': type.value + ' ' + token.value
+        }
+    })
+    alert('收藏成功!')
 }
 </script>
 <template>
     <!-- 篩選條件 -->
-    <div class="flex flex-col justify-center gap-10">
+    <div v-if="showFilters" class="flex flex-col justify-center gap-10">
         <div
             class="flex items-center justify-center gap-20 w-full h-auto pt-4 mx-auto text-2xl text-black bg-[#d1fcdb]">
             <div class="max-w-screen flex flex-col gap-5">
@@ -128,13 +193,35 @@ async function courseSearch() {
         </div>
     </div>
     <!-- 搜尋結果表格 -->
-    <div class="mt-10 grid border h-auto  grid-cols-13 overflow-auto ">
-        <div v-for="(item, index) in courseArray" :key="index"
-            :class="index < 13 ? 'h-10 bg-[#EEEEEE] font-bold ' : 'h-16 bg-white'"
-            class=" flex flex-col border border-[#146d2b] items-center justify-center">
-            <div>{{ item }}</div>
+    <div v-if="showTable" class="relative mt-10 flex flex-col items-center overflow-auto w-full gap-5">
+        <div
+            class="flex gap-2 items-center justify-end w-[calc(60px+100px+180px+40px+400px+100px+80px+40px+200px+100px+40px+100px+60px)]">
+            <button class="bg-[#6ffc92] w-30 h-10 border rounded cursor-pointer hover:bg-[#23f157] hover:font-bold"
+                @click="showFilters = !showFilters">{{ showFilters ? '隱藏條件' : '顯示條件' }}</button>
+            <button class="bg-[#6ffc92] w-30 h-10 border rounded cursor-pointer hover:bg-[#23f157] hover:font-bold"
+                @click="downloadExcel()">匯出</button>
+        </div>
+        <!-- <div class="absolute right-2 top-0">
+            <button class="w-30 h-10 border rounded-lg bg-[#23f157] cursor-pointer hover:bg-[#18a83c] hover:font-bold" @click="showFilters = !showFilters">{{ showFilters? '隱藏條件' : '顯示條件' }}</button>
+        </div> -->
+        <div
+            class=" inline-grid h-auto grid-cols-[60px_100px_180px_40px_400px_100px_80px_40px_200px_100px_40px_100px_60px]  ">
+            <div v-for="(item, index) in courseArray" :key="index"
+                :class="index < 13 ? 'h-10 bg-[#74EF93] font-bold ' : 'h-16 bg-[#EEEEEE]'"
+                class=" flex flex-col border border-[#146d2b] items-center justify-center">
+                <div :class="index > 13 && index % 13 === 12 ? 'hidden' : ''">{{ item }}</div>
+                <svg @click="addFavorite(Math.floor((index - 13) / 13))"
+                    :class="index > 13 && index % 13 === 12 ? `${favoriteArray[Math.floor((index - 13) / 13)] ? 'text-[#ffff00]' : 'text-white hover:text-[#ffff00] cursor-pointer'}` : 'hidden'"
+                    xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="currentColor"
+                    stroke="black" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+                    <polygon
+                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+                    </polygon>
+                </svg>
+            </div>
         </div>
     </div>
+
     <!-- 時間選擇 -->
     <div v-if="showChooceTime" class=" fixed flex items-center justify-center inset-0 bg-black/50 z-40 ">
         <div
